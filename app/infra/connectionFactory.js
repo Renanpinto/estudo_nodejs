fef0446735de564cb52fc83092a0b28a79ea4780
Renan Pinto <renanspinto@hotmail.com>
2018-06-10 22:59:20 -0300

    utilizando body-parser

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -10,6 +10,7 @@ function createDBConnection() {
     })
 }
 
+//WRAPPER
 module.exports = function (){
     return createDBConnection;
 
--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,24 +1,40 @@
 
 module.exports = (app) => {
-    
+
     app.get('/',
         (req, res) => res.send("bagulhos")
     )
 
     app.get('/produtos',
         (req, res) => {
-           
-            const connection = app.infra.connectionFactory();
-            
-            connection.query('select * from livros', (err, results) => {
-                res.render("produtos/lista", {lista: results})
+
+            const connection = app.infra.connectionFactory()
+            const produtosBanco = new app.infra.ProdutosDAO(connection)
+
+            produtosBanco.lista((err, results) => {
+                res.render("produtos/lista", { lista: results })
                 // res.send(livrosJson[0])        
             })
 
-            
+
             connection.end()
 
             // res.render("produtos/lista")
         }
+
+    )
+    app.get('/produtos/form',
+        (req, res) => res.render("produtos/form")
+    )
+    app.post('/produtos', (req, res) => {
+        const connection = app.infra.connectionFactory()
+        const produtosDao = new app.infra.ProdutosDAO(connection)
+
+        const produto = req.body
+        produtosDao.salva(produto, (err, results) => {
+            res.redirect("/produtos")
+        })
+        connection.end()
+    }
     )
 }
